4829f48014906fe0c65adeaf5949451ccb056e37
alvaropsouza <alvaro.patricio.dev@gmail.com>
2026-04-09 16:23:23 -0300

    refactor(app): remove unused ServeStaticModule import and configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
-import { join } from 'node:path';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(process.cwd(), 'public'),
-    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
